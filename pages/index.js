7ad78adc01e868f8b20143db9be63a628cb8474e
Khoury Smith <khoury.smith@gmail.com>
2022-02-16 15:10:29 -0500

    added email page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Deck from "../components/Deck";
+import Email from "../components/Email";
 
 export default function Home() {
-  return <Deck />;
+  return <Email />;
 }
